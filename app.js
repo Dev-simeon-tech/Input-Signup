9b52b0fc32534f8528ba6f85a5df46993ccab3f4
Dev simeon <elebesunuosereme@gmail.com>
2023-09-02 15:46:53 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const date = document.querySelector(".date")
+const presentYear = new Date().getFullYear()
+date.innerHTML = presentYear;
+const form = document.getElementById('form')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const lastName = document.getElementById('last-name')
+const firstName = document.getElementById('first-name')
+const label = document.querySelector('.error-label')
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    Validation();
+    
+})
+
+
+const setError = (object,message)=>{
+    inputBox = object.parentElement;
+    const errorDis = inputBox.querySelector('.error-msg')
+    const input = inputBox.querySelector('.input')
+    input.placeholder = ''
+    input.style.border = '1px solid hsl(0, 100%, 74%)'
+    input.classList.add('color')
+    errorDis.innerText = message
+    const errorImg = inputBox.querySelector('.error-img')
+    errorImg.classList.add('active')
+   
+    if(object = email){
+        email.placeholder = "email@example/com"
+        // email.placeholder.style.color = 'red'
+     
+    }
+    else{
+        email.placeholder = ''
+    }
+
+   
+}
+const setPass = (object) =>{
+    inputBox = object.parentElement;
+    const errorDis = inputBox.querySelector('.error-msg')
+    errorDis.innerText = ''
+    const errorImg = inputBox.querySelector('.error-img')
+    errorImg.classList.remove('active')
+
+    const input = inputBox.querySelector('.input')
+    input.style.border = 'none'
+    
+
+}
+
+
+const Validation = function(){
+    if(firstName.value === ''){
+        setError(firstName, "First name connot be empty");
+    }
+    else{
+        setPass(firstName);
+        
+    }
+    
+    if(lastName.value === ''){
+        setError(lastName,"Last name connot be empty");
+
+    }
+    else{
+        setPass(lastName)
+    }
+
+    if(email.value === ""){
+        setError(email,"Looks like this is not an email");
+
+    }
+    else{
+        setPass(email);
+    }
+    if(password.value ===''){
+        setError(password,"Password cannot be empty")
+    }
+    else{
+        setPass(password)
+    }
+  
+}
